82eec4861320ad7fd3f472879a29edabb034ff0d
NagyLP <nagy.peter@pr.hu>
2022-01-31 11:28:11 +0100

    Dalok

--- src/main/java/streams/SongService.java
+++ src/main/java/streams/SongService.java
@@ -1,8 +1,37 @@
 package streams;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class SongService {
 
-    private final List<>
+    private final List<Song> songs = new ArrayList<>();
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public Optional<Song> shortestSong() {
+        return songs.stream().min(Comparator.comparing(Song::getLength));
+    }
+
+    List<Song> findSongByTitle(String title) {
+        return songs.stream()
+                .filter(song -> song.getTitle().equals(title))
+                .toList();
+    }
+
+    public boolean isPerformerInSong(Song song, String performer) {
+        return song.getPerformers().stream().anyMatch(p -> p.equals(performer));
+    }
+
+    public List<String> titlesBeforeDate(LocalDate date) {
+        return songs.stream()
+                .filter(song -> song.getRelease().isBefore(date))
+                .map(Song::getTitle)
+                .toList();
+    }
 }
