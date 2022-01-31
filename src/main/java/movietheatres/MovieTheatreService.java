b7212695fb312c839964fed6628c6e86635165e7
NagyLP <nagy.peter@pr.hu>
2022-01-31 12:01:40 +0100

    Moziműsor I.

--- src/main/java/movietheatres/MovieTheatreService.java
+++ src/main/java/movietheatres/MovieTheatreService.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MovieTheatreService {
 
@@ -35,17 +32,6 @@ public class MovieTheatreService {
             shows.put(parts[0], new ArrayList<>());
         }
         shows.get(parts[0]).add(new Movie(partsTwoo[0], LocalTime.parse(parts[1])));
+        shows.get(parts[0]).sort(Comparator.comparing(Movie::getStartTime));
     }
-
-    private void parseLine(String item) {
-        String[] parts = item.split("-");
-        String[] partsTwoo = parts[1].split(";");
-        if (!shows.containsKey(parts[0])) {
-            shows.put(parts[0], new ArrayList<>());
-        }
-        shows.get(parts[0]).sort();
-    }
-
-
-
 }
